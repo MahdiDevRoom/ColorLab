75fc8d3bc14c818fa22e7da0f49854e4cafc4138
MahdiDevRoom <mahdidevroom@gmail.com>
2025-09-23 15:03:24 +0330

    update

---
+++ scripts/main.js
@@ -0,0 +1,29 @@
+let toggleMenu = document.querySelector('#toggle-menu');
+let menu = document.querySelector('header .menu');
+let toggleTheme = document.querySelector('#toggle-theme');
+let themeColor = document.querySelector('#theme-color');
+let storage = JSON.parse(localStorage.getItem('storage'));
+let changeTheme = (theme) => {
+   themeColor.href = `styles/theme/${theme}.css`;
+   toggleTheme.querySelector('.symbol').innerHTML = `${theme == 'light' ? 'dark' : 'light'}_mode`;
+}
+
+if (!storage) {
+   storage = {theme: 'light'};
+   localStorage.setItem('storage',JSON.stringify(storage));
+}
+
+changeTheme(storage.theme);
+
+toggleMenu.onclick = ()=> {
+   menu.classList.toggle('open');
+}
+   
+
+
+toggleTheme.onclick = ()=> {
+   storage = {theme: storage.theme == 'light' ? 'dark' : 'light' };
+   localStorage.setItem('storage',JSON.stringify(storage));
+   changeTheme(storage.theme);
+}
+   
